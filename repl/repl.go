bc54f44ac992ff0f52ac252b97a88475cdd28d2c
gkivivali <georgkivivali7@gmail.com>
2024-02-07 23:56:39 +0200

    Working on evaluator, integers, booleans working

--- repl/repl.go
+++ repl/repl.go
@@ -3,8 +3,9 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"interpreter/evaluator"
 	"interpreter/lexer"
-    "interpreter/parser"
+	"interpreter/parser"
 	"io"
 )
 
@@ -22,24 +23,24 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
-        p := parser.New(l)
+		p := parser.New(l)
 
-        program := p.ParseProgram()
-        
-        if len(p.Errors()) != 0 {
-            printParserErrors(out, p.Errors())
-            continue
-        }
-        io.WriteString(out, program.String())
-        io.WriteString(out, "\n")
-		// for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-		// 	fmt.Printf("%+v\n", tok)
-		// }
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
 func printParserErrors(out io.Writer, errors []string) {
-    for _, msg := range errors {
-       io.WriteString(out, "\t"+msg+"\n") 
-    }
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
